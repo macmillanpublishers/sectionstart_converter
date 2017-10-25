41c2b008b47d6f024381623aa716308835b8bbc2
MacmillanWorkflows <workflows@macmillan.com>
2017-10-24 22:23:54 -0400

    added dbox api to test

--- xml_docx_stylechecks/shared_utils/filenametest.py
+++ xml_docx_stylechecks/shared_utils/filenametest.py
@@ -9,11 +9,49 @@ import re
 # import logging
 import time
 import getpass
+import dropbox
+
+# if __name__ == '__main__':
+# 	# to go up a level to read cfg when invoking from this script (for testing).
+#     setupcleanuppath = os.path.join(sys.path[0], '..', 'lib', 'setup_cleanup.py')
+#     import imp
+#     parentpath = os.path.join(sys.path[0], '..', 'cfg.py')
+#     cfg = imp.load_source('cfg', parentpath)
+#     setup_cleanup = imp.load_source('os_utils', setupcleanuppath)
+# else:
+#     import cfg
+def getSubmitterViaAPI(inputfile):
+    # logger.info("Retrieve submitter info via Dropbox api...")
+    submitter_email = ""
+    display_name = ""
+    dropboxfolder = os.path.join("C:",os.sep,"Users",currentuser,"Dropbox (Macmillan Publishers)")
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    db_access_token_txt = os.path.join(__location__,'..','..','..',"bookmaker_authkeys","access_token.txt")
+    with open(db_access_token_txt) as f:
+        db_access_token = f.readline()
+    try:
+        # dropbox api requires forward slash in path, and is a relative path (in relation to Dropbox folder)
+        dropbox_relpath = inputfile.replace(dropboxfolder,"").replace("\\","/")
+        dbx = dropbox.Dropbox(db_access_token)
+        submitter = (dbx.files_get_metadata(dropbox_relpath).sharing_info.modified_by)
+        display_name = dbx.users_get_account(submitter).name.display_name
+        submitter_email = dbx.users_get_account(submitter).email
+    except Exception, e:
+        print e
+    finally:
+        return submitter_email, display_name
+
+
 # from lxml import etree
 badchar = sys.argv[1]
 inputfile = sys.argv[2]
 inputfilename = os.path.basename(inputfile)
 inputfilename_noext = os.path.splitext(inputfilename)[0]
+inputfilenq = inputfile
+if inputfile[0] == '"':
+    inputfilenq = inputfile[1:]
+if inputfilenq[-1:] == '"':
+    inputfilenq = inputfilenq[:-1]
 
 currentuser = getpass.getuser()
 # filename = "S:\Users\padwoadmin\Documents\programming_projects\1708_2_python_ssconvertertests\bugsrug_%s.txt" % time.strftime("%m%d-%H%M%S")
@@ -24,7 +62,8 @@ try:
     print "inputfilename_noext : ", inputfilename_noext
     print "inputfilename : ", inputfilename
     print "inputfile : ", inputfile
-    print "inputfilenamenoext encode : ", inputfilename_noext.decode('hex')#.encode("ascii")
+    print getSubmitterViaAPI(inputfilenq)
+    # print "inputfilenamenoext encode : ", inputfilename_noext.decode('hex')#.encode("ascii")
 
     newfile = open(filename, 'a')
     with newfile as f:
@@ -34,7 +73,7 @@ try:
         f.write("inputfilename_noext : %s\n" % inputfilename_noext)
         f.write("inputfilename : %s\n" % inputfilename)
         f.write("inputfile : %s\n" % inputfile)
-        f.write("inputfilenamenoext encode : %s\n" % inputfilename_noext.decode("cp1250"))#.encode('utf8'))
+        # f.write("inputfilenamenoext encode : %s\n" % inputfilename_noext.decode("cp1250"))#.encode('utf8'))
         f.write("____________________\n")
             # f.write(line)
         f.close()

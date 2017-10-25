a3b98a7dbd2524a6c0ecc4d939d1630ca64a202c
MacmillanWorkflows <workflows@macmillan.com>
2017-10-25 16:30:26 -0400

    finished unicode support, added tests for file move & copy

--- xml_docx_stylechecks/shared_utils/filenametest.py
+++ xml_docx_stylechecks/shared_utils/filenametest.py
@@ -20,6 +20,32 @@ import dropbox
 #     setup_cleanup = imp.load_source('os_utils', setupcleanuppath)
 # else:
 #     import cfg
+newhome = os.path.join("S:",os.sep,"Users","padwoadmin","Documents","programming_projects","1708_2_python_ssconvertertests","newhome")
+copydest = os.path.join("S:",os.sep,"Users","padwoadmin","Documents","programming_projects","1708_2_python_ssconvertertests","copydest")
+def moveFile(pathtofile, dest):
+    # Create dest directory if it does not exist (guessing whether we have a file or dir as dest, based on period in name)
+    if "." not in dest:
+        if not os.path.isdir(dest):
+            os.makedirs(dest)
+    else:
+        if not os.path.isdir(os.path.dirname(dest)):
+            os.makedirs(os.path.dirname(dest))
+    # Move the file
+    # try:
+    shutil.move(pathtofile, dest)
+    # except Exception, e:
+    #     logger.error('Failed to move file, exiting', exc_info=True)
+    #     sys.exit(1)
+
+def copyFiletoFile(pathtofile, dest_file):
+    if not os.path.isdir(os.path.dirname(dest_file)):
+        os.makedirs(os.path.dirname(dest_file))
+    # try:
+    shutil.copyfile(pathtofile, dest_file)
+    # except Exception, e:
+    #     logger.error('Failed copyfile, exiting', exc_info=True)
+    #     sys.exit(1)
+
 def getSubmitterViaAPI(inputfile):
     # logger.info("Retrieve submitter info via Dropbox api...")
     submitter_email = ""
@@ -29,17 +55,22 @@ def getSubmitterViaAPI(inputfile):
     db_access_token_txt = os.path.join(__location__,'..','..','..',"bookmaker_authkeys","access_token.txt")
     with open(db_access_token_txt) as f:
         db_access_token = f.readline()
-    try:
-        # dropbox api requires forward slash in path, and is a relative path (in relation to Dropbox folder)
-        dropbox_relpath = inputfile.replace(dropboxfolder,"").replace("\\","/")
-        dbx = dropbox.Dropbox(db_access_token)
-        submitter = (dbx.files_get_metadata(dropbox_relpath).sharing_info.modified_by)
-        display_name = dbx.users_get_account(submitter).name.display_name
-        submitter_email = dbx.users_get_account(submitter).email
-    except Exception, e:
-        print e
-    finally:
-        return submitter_email, display_name
+    # try:
+    # dropbox api requires forward slash in path, and is a relative path (in relation to Dropbox folder)
+    # the decode cp1252 is to unencode unicode chars that were encoded by the batch file, for the api.
+    dropbox_relpath = inputfile.replace(dropboxfolder,"").replace("\\","/").decode("cp1252")
+    dbx = dropbox.Dropbox(db_access_token)
+    submitter = (dbx.files_get_metadata(dropbox_relpath).sharing_info.modified_by)
+    display_name = dbx.users_get_account(submitter).name.display_name
+    submitter_email = dbx.users_get_account(submitter).email
+# except Exception, e:
+#     print e#.decode("utf-8")
+#     newfile = open(filename, 'a')
+#     with newfile as f:
+#         f.write("{}\n".format(e))
+#         f.close()
+# finally:
+    return submitter_email, display_name
 
 
 # from lxml import etree
@@ -54,41 +85,34 @@ if inputfilenq[-1:] == '"':
     inputfilenq = inputfilenq[:-1]
 
 currentuser = getpass.getuser()
-# filename = "S:\Users\padwoadmin\Documents\programming_projects\1708_2_python_ssconvertertests\bugsrug_%s.txt" % time.strftime("%m%d-%H%M%S")
 filename = os.path.join(os.sep,"Users",currentuser,"Documents","programming_projects","1708_2_python_ssconvertertests","bugsrug_.txt")
-# filename = os.path.join(os.sep,"Users",currentuser,"Documents","programming_projects","1708_2_python_ssconvertertests","bugsrug_%s.txt"% time.strftime("%m%d-%H%M%S"))
-try:
-    print "version / badchar: ",badchar
-    print "inputfilename_noext : ", inputfilename_noext
-    print "inputfilename : ", inputfilename
-    print "inputfile : ", inputfile
-    print getSubmitterViaAPI(inputfilenq)
-    # print "inputfilenamenoext encode : ", inputfilename_noext.decode('hex')#.encode("ascii")
+# try:
+submitterA, submitterB = getSubmitterViaAPI(inputfilenq)
+# print "version / badchar: ",badchar
+print "inputfilename_noext : ", inputfilename_noext
+print "inputfilename : ", inputfilename
+print "inputfile : ", inputfile
+print submitterA, submitterB
+# print "inputfilenamenoext encode : ", inputfilename_noext.decode('hex')#.encode("ascii")
 
-    newfile = open(filename, 'a')
-    with newfile as f:
-        # for line in text:
-        #     f.write("%s\n" % line)
-        f.write("version / badchar: %s\n" % badchar)
-        f.write("inputfilename_noext : %s\n" % inputfilename_noext)
-        f.write("inputfilename : %s\n" % inputfilename)
-        f.write("inputfile : %s\n" % inputfile)
-        # f.write("inputfilenamenoext encode : %s\n" % inputfilename_noext.decode("cp1250"))#.encode('utf8'))
-        f.write("____________________\n")
-            # f.write(line)
-        f.close()
-except Exception, e:
-    print e
-    newfile = open(filename, 'a')
-    with newfile as f:
-        # for line in text:
-        #     f.write("%s\n" % line)
+newfile = open(filename, 'a')
+with newfile as f:
+    # for line in text:
+    f.write("version / badchar: {}\n".format(badchar))
+    f.write("inputfilename_noext : {}\n".format(inputfilename_noext))
+    f.write("inputfilename : {}\n".format(inputfilename))
+    f.write("inputfile : {}\n".format(inputfile))
+    f.write("{}, {}\n".format(submitterA, submitterB))
+    f.write("____________________\n")
+        # f.write(line)
+    f.close()
+# except Exception, e:
+#     print e#.decode("utf-8")
+#     newfile = open(filename, 'a')
+#     with newfile as f:
+#         f.write("{}\n".format(e))
+#         f.close()
+sanitizedname = re.sub('\W','',inputfilename_noext)
+copyFiletoFile(inputfilenq, os.path.join(copydest,"%s.docx" %sanitizedname))
 
-        f.write("%s\n" % e)
-        # f.write("inputfilename_noext : %s\n" % inputfilename_noext)
-        # f.write("inputfilename : %s\n" % inputfilename)
-        # f.write("inputfile : %s\n" % inputfile)
-        # f.write("inputfilenamenoext encode : %s\n" % inputfilename_noext.encode('utf8'))
-        # f.write("____________________\n")
-        #     # f.write(line)
-        f.close()
+moveFile(inputfilenq, newhome)
